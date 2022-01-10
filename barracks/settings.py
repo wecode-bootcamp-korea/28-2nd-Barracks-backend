44c1a8cb76ef7f8a9b670628928495c3ef9b5710
AhYoung Lee <dkdud5135@naver.com>
2022-01-10 18:00:57 +0900

    Modify : settings.py 수정

--- barracks/settings.py
+++ barracks/settings.py
@@ -1,9 +1,9 @@
 from pathlib     import Path
 from my_settings import DATABASES, SECRET_KEY
 
-# import pymysql
+import pymysql
 
-# pymysql.install_as_MySQLdb()
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
